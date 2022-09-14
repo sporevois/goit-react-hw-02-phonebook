c5fbe13371033c5f0e5f1fab825650d249216376
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-09-15 01:10:38 +0300

    creates form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import Section from "./Section/Section";
+
+export class App extends Component {
+
+  state = {
+    contacts: [],
+    name: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    // const { name } = this.state;
+    // this.props.onSubmit({ ...this.state });
+    this.setState({
+      name: ''
+    })
+  }
+
+  handleChange = (event) => {
+    this.setState({ name: event.target.value });
+  }
+
+  render() {
+    const { name } = this.state;
+     return (
+      <>
+        <Section title="Phonebook">
+          <form onSubmit={this.handleSubmit}>
+            <label>
+              Name
+              <input
+                type="text"
+                name="name"
+                pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                required
+                value = {name}
+                onChange={this.handleChange}
+              />
+            </label>
+            <button type="submit">Add contact</button>
+          </form>
+        </Section>
+        <Section title="Contacts">
+        
+        </Section>
+      </>
+    )
+  }
 };
